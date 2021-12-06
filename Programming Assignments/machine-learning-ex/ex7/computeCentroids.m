f9c5368885b1922f77ee2a6677a8dc863e74744d
ishaan-narula <60092550+ishaan-narula@users.noreply.github.com>
2021-12-06 15:06:13 +0530

    Updated files

---
+++ Programming Assignments/machine-learning-ex/ex7/computeCentroids.m
@@ -0,0 +1,41 @@
+function centroids = computeCentroids(X, idx, K)
+%COMPUTECENTROIDS returns the new centroids by computing the means of the 
+%data points assigned to each centroid.
+%   centroids = COMPUTECENTROIDS(X, idx, K) returns the new centroids by 
+%   computing the means of the data points assigned to each centroid. It is
+%   given a dataset X where each row is a single data point, a vector
+%   idx of centroid assignments (i.e. each entry in range [1..K]) for each
+%   example, and K, the number of centroids. You should return a matrix
+%   centroids, where each row of centroids is the mean of the data points
+%   assigned to it.
+%
+
+% Useful variables
+[m n] = size(X);
+
+% You need to return the following variables correctly.
+centroids = zeros(K, n);
+
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Go over every centroid and compute mean of all points that
+%               belong to it. Concretely, the row vector centroids(i, :)
+%               should contain the mean of the data points assigned to
+%               centroid i.
+%
+% Note: You can use a for-loop over the centroids to compute this.
+%
+
+% Creating a temporary matrix X
+X_temp = [X idx]
+
+for i = 1:K
+    ind_i = X_temp(:, end) == i; % A vector of 0s and 1s wherein 1 corresponds to those coordinates/ rows in X which have been assigned to centroid i
+    X_subset = X(ind_i, :); % A subset of the X matrix containing only those coordinates/ rows which belong to centroid i
+    centroids(i, :) = sum(X_subset, 1)/size(X_subset, 1); % Mean of the coordinates in X_subset
+
+% =============================================================
+
+
+end
+
